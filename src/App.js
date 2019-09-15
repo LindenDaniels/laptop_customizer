ee5d393a58d08e85956eaeaa2ab2e5488668a4ba
Linden Daniels <lindendaniels@gmail.com>
2019-09-14 20:56:43 -0400

    update files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import LaptopFeatures from './LaptopFeatures/LaptopFeatures';
+
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// This object will allow us to
+// easily convert numbers into US dollar values
+
+
+class App extends Component {
+  state = {
+    selected: {
+      Processor: {
+        name: '17th Generation Intel Core HB (7 Core with donut spare)',
+        cost: 700
+      },
+      'Operating System': {
+        name: 'Ubuntu Linux 16.04',
+        cost: 200
+      },
+      'Video Card': {
+        name: 'Toyota Corolla 1.5v',
+        cost: 1150.98
+      },
+      Display: {
+        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+        cost: 1500
+      }
+    }
+  }
+  
+  updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <LaptopFeatures />
+      
+      </div>
+    );
+  }
+  }
 
 export default App;
